b89d79141dee4fa35a4e9984b415995a8358d67e
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-01-30 00:56:23 +0600

    feat: add footer component

--- client/src/layout/Root.jsx
+++ client/src/layout/Root.jsx
@@ -4,10 +4,12 @@ import Footer from "../components/Footer";
 
 const Root = () => {
   return (
-    <div>
-      <NavBar/>
-      <Outlet />
-      <Footer />      
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
