08d7fa6f1a5023d197f69212c1b51ea556bb5b6f
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-01-30 15:23:16 +0600

    feat: add equip cards on homepage

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -14,6 +14,7 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        loader: () => fetch("http://localhost:5000/sports"),
       },
       {
         path: "/add-equips",
